9d86d3328c88c0c0bb7289d8622fbf07f3f51753
JWicquart <jeremywicquart@gmail.com>
2025-07-22 09:17:25 +0200

    plot dhw percent

--- code/b04_extract_crw.R
+++ code/b04_extract_crw.R
@@ -11,13 +11,11 @@ source("code/function/extract_coeff.R")
 
 plan(multisession, workers = 4) # Set parallelization with 4 cores
 
-# 2. Load and transform coral reefs ----
+# 2. Load and transform data ----
 
-## 2.1 Load file ----
+## 2.1 Transform coral reefs ----
 
 data_reefs <- st_read("data/01_maps/02_clean/01_reefs/reefs.shp")
-
-## 2.2 Add regions and summarise ----
 
 data_reefs_a <- data_reefs %>% 
   group_by(region) %>%
@@ -25,23 +23,31 @@ data_reefs_a <- data_reefs %>%
   ungroup() %>% 
   mutate(subregion = "All")
 
-## 2.3 Add global reefs ----
-
 data_reefs_b <- data_reefs %>% 
   summarise(do_union = TRUE) %>% 
   mutate(region = "All", subregion = "All")
-
-## 2.4 Bind shp ----
 
 data_reefs <- data_reefs %>% 
   bind_rows(., data_reefs_a) %>% 
   bind_rows(., data_reefs_b)
 
-## 2.5 Remove useless objects ----
-
 rm(data_reefs_a, data_reefs_b)
 
-# 3. Extract yearly SST anomaly ----
+## 2.2 Transform regions ----
+
+data_regions <- st_read("data/01_maps/02_clean/03_regions/gcrmn_regions.shp")
+
+data_subregions <- st_read("data/01_maps/02_clean/04_subregions/gcrmn_subregions.shp")
+
+data_vect <- data_regions %>% 
+  summarise(do_union = TRUE) %>% 
+  mutate(region = "All", subregion = "All") %>% 
+  bind_rows(., data_regions %>% mutate(subregion = "All")) %>% 
+  bind_rows(., data_subregions)
+
+rm(data_regions, data_subregions)
+
+# 3. Extract yearly mean SST anomaly ----
 
 ## 3.1 List of files ----
 
@@ -110,8 +116,78 @@ data_ssta_mean_year <- map(1:nrow(list_url), ~extract_ssta_mean_year(row_nb = .,
 
 save(data_ssta_mean_year, file = "data/02_misc/data_ssta_mean_year.RData")
 
+# 4. Extract yearly DHW ----
 
-# dhw percent
-# daily sst
+## 4.1 List of files ----
 
+list_url <- data.frame(year = seq(from = 1986, to = 2024, by = 1)) %>% 
+  mutate(url = paste0("https://www.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/annual/ct5km_",
+                      "dhw-max",
+                      "_v3.1_",
+                      year,
+                      ".nc"),
+         filename = str_split_fixed(url, "/", Inf)[,14])
 
+## 4.2 Function to download raster and extract values ----
+
+extract_dhw_max_percent_year <- function(row_nb, data_reef = data_reefs, data_vect = data_vect){
+  
+  # A. Remove files in folder
+  
+  file.remove(list.files("data/04_crw/", full.names = TRUE))
+  
+  # B. Download file
+  
+  list_url_i <- list_url %>% 
+    filter(row_number(.) == row_nb)
+  
+  download.file(url = list_url_i[1, "url"],
+                destfile = paste0("data/04_crw/", list_url_i[1, "filename"]),
+                mode = "wb",   # Use mode "wb" for windows otherwise issue to read the file with terra
+                timeout = max(600, getOption("timeout"))) # 600 seconds to download the file, else error message
+  
+  # C. Load the raster
+  
+  ncdf <- terra::rast(paste0("data/04_crw/", list_url_i[1, "filename"]))
+  
+  ncdf <- ncdf$degree_heating_week
+  
+  crs(ncdf) <- "epsg:4326"
+  
+  # D. Extract values
+  
+  ncdf_crop <- crop(ncdf, data_reefs, mask = TRUE)
+  
+  data_results <- freq(ncdf_crop, zones = vect(data_vect)) %>% 
+    dplyr::mutate(year = year(unique(time(ncdf)))) %>% 
+    left_join(., data_vect %>% st_drop_geometry() %>% mutate(zone = row_number())) %>% 
+    select(-layer, -zone) %>% 
+    rename(dhw = value, nb_cells = count)
+  
+  # E. Delete raw file
+  
+  file.remove(paste0("data/04_crw/", list_url_i[1, "filename"]))
+  
+  # F. Return the results
+  
+  return(data_results)
+  
+}
+
+## 4.3 Map over the function ----
+
+data_dhw_freq <- map(1:nrow(list_url),
+                     ~extract_dhw_max_percent_year(row_nb = ., data_reef = data_reefs, data_vect = data_vect)) %>% 
+  list_rbind()
+
+## 4.4 Save the data ----
+
+save(data_dhw_freq, file = "data/02_misc/data_dhw_freq.RData")
+
+# 5. Extract daily mean SST ----
+
+## 5.1 List of files ----
+
+## 5.2 Function to download raster and extract values ----
+
+## 5.3. Map over the function ----
--- code/e05_crw.R
+++ code/e05_crw.R
@@ -26,7 +26,8 @@ plot_i <- ggplot(data = data_ssta_mean_year %>% filter(region == "All")) +
   theme(plot.title = element_markdown(size = 17, face = "bold", family = "Open Sans Semibold"),
         plot.subtitle = element_markdown(size = 12)) +
   labs(x = "Year", y = "SST anomaly (°C)") +
-  theme_graph()
+  theme_graph() +
+  lims(y = c(-1.5, 1.5))
 
 ggsave("figs/01_part-1/fig-10.png", height = 5.3, width = 7.2, dpi = fig_resolution)
 
@@ -41,7 +42,8 @@ plot_ssta <- function(region_i){
     theme(plot.title = element_markdown(size = 17, face = "bold", family = "Open Sans Semibold"),
           plot.subtitle = element_markdown(size = 12)) +
     labs(x = "Year", y = "SST anomaly (°C)") +
-    theme_graph()
+    theme_graph() +
+    lims(y = c(-1.5, 1.5))
 
   ggsave(filename = paste0("figs/02_part-2/fig-3/",
                            str_replace_all(str_replace_all(str_to_lower(region_i), " ", "-"), "---", "-"), ".png"),
@@ -51,5 +53,68 @@ plot_ssta <- function(region_i){
 
 map(setdiff(unique(data_ssta_mean_year$region), "All"), ~plot_ssta(region_i = .))
 
+# 4. DHW percent ----
 
+load("data/02_misc/data_dhw_freq.RData")
 
+data_dhw_freq <- data_dhw_freq %>% 
+  complete(year, dhw, nesting(region, subregion), fill = list(nb_cells = 0)) %>% 
+  # See https://coralreefwatch.noaa.gov/product/5km/index_5km_baa-max-7d.php
+  mutate(heatstress = case_when(dhw <= 0 ~ "No Stress",
+                                #dhw > 0 & dhw < 1 ~ "Bleaching Watch",
+                                dhw >= 1 & dhw < 4 ~ "Warning",
+                                dhw >= 4 & dhw < 8 ~ "Alert 1",
+                                dhw >= 8 & dhw < 12 ~ "Alert 2",
+                                dhw >= 12 & dhw < 16 ~ "Alert 3",
+                                dhw >= 16 & dhw < 20 ~ "Alert 4",
+                                dhw >= 20 ~ "Alert 5")) %>% 
+  group_by(year, region, subregion, heatstress) %>% 
+  summarise(nb_cells = sum(nb_cells)) %>% 
+  ungroup() %>% 
+  group_by(year, region, subregion) %>% 
+  mutate(total_nb_cells = sum(nb_cells)) %>% 
+  ungroup() %>% 
+  mutate(freq = (nb_cells*100)/total_nb_cells,
+         heatstress = as.factor(heatstress),
+         heatstress = factor(heatstress, levels = c("No Stress", "Warning",
+                                                    "Alert 1", "Alert 2", "Alert 3", "Alert 4", "Alert 5")))
+
+## 4.1 Global ----
+
+plot_i <- ggplot(data = data_dhw_freq %>% filter(region == "All")) +
+  geom_bar(aes(x = year, y = freq, fill = heatstress), stat = "identity") +
+  scale_fill_manual(values = c("No Stress" = "lightgrey",
+                               "Warning" = palette_second[1],
+                               "Alert 1" = palette_second[2],
+                               "Alert 2" = palette_second[3],
+                               "Alert 3" = palette_second[4],
+                               "Alert 4" = palette_second[5],
+                               "Alert 5" = "black")) +
+  theme_graph() +
+  labs(x = "Year", y = "Percentage of coral reefs")
+
+ggsave("figs/01_part-1/fig-11.png", height = 5.3, width = 7.2, dpi = fig_resolution)
+
+## 4.2 Regional ----
+
+plot_dhw <- function(region_i){
+  
+  plot_i <- ggplot(data = data_dhw_freq %>% filter(region == region_i & subregion == "All")) +
+    geom_bar(aes(x = year, y = freq, fill = heatstress), stat = "identity") +
+    scale_fill_manual(values = c("No Stress" = "lightgrey",
+                                 "Warning" = palette_second[1],
+                                 "Alert 1" = palette_second[2],
+                                 "Alert 2" = palette_second[3],
+                                 "Alert 3" = palette_second[4],
+                                 "Alert 4" = palette_second[5],
+                                 "Alert 5" = "black")) +
+    theme_graph() +
+    labs(x = "Year", y = "Percentage of coral reefs")
+  
+  ggsave(filename = paste0("figs/02_part-2/fig-4/",
+                           str_replace_all(str_replace_all(str_to_lower(region_i), " ", "-"), "---", "-"), ".png"),
+         plot = plot_i, height = 5.3, width = 7.2, dpi = fig_resolution)
+  
+}
+
+map(setdiff(unique(data_dhw_freq$region), "All"), ~plot_dhw(region_i = .))
