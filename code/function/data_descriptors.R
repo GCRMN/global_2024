11148ad597dc3a50e09b8b6f9d0034159156c9fe
JWicquart <jeremywicquart@gmail.com>
2025-07-31 13:07:18 +0200

    return indicator surveys_90_perc

--- code/function/data_descriptors.R
+++ code/function/data_descriptors.R
@@ -29,6 +29,22 @@ data_descriptors <- function(data){
     select(first_year, last_year) %>% 
     distinct()
   
+  # Year from which 90% of surveys were assessed
+  
+  surveys_90_perc <- data %>% 
+    select(decimalLongitude, decimalLatitude, year, month, eventDate) %>%
+    distinct() %>% 
+    group_by(year, .add = TRUE) %>% 
+    count() %>% 
+    ungroup(year) %>% 
+    mutate(perc = (n*100)/sum(n)) %>% 
+    arrange(-year) %>% 
+    mutate(cumsum = cumsum(perc)) %>% 
+    filter(cumsum >= 90) %>% # Put the threshold here
+    filter(cumsum == min(cumsum)) %>% 
+    select(year) %>% 
+    rename(surveys_90_perc = year)
+  
   # Return the results
   
   if (is.grouped_df(data) == TRUE) {
@@ -36,11 +52,12 @@ data_descriptors <- function(data){
     result <- nb_datasets %>% 
       left_join(., nb_sites) %>% 
       left_join(., nb_surveys) %>% 
-      left_join(., first_last_year)
+      left_join(., first_last_year) %>% 
+      left_join(., surveys_90_perc)
     
   }else{
     
-    result <- bind_cols(nb_datasets, nb_sites, nb_surveys, first_last_year)
+    result <- bind_cols(nb_datasets, nb_sites, nb_surveys, first_last_year, surveys_90_perc)
     
   } 
   
