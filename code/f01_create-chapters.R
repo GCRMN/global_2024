00567839ad787c272afc19deee888f0ab1657aba
JWicquart <jeremywicquart@gmail.com>
2025-08-08 11:52:59 +0200

    major update on chapter production

--- code/f01_create-chapters.R
+++ code/f01_create-chapters.R
@@ -5,14 +5,45 @@ library(rmarkdown)
 library(sf)
 library(googledrive)
 
-# 2. Load functions ----
-
-source("code/function/render_qmd.R")
-
-# 3. Create docx files for each area ----
+# 2. Load the regions to map over ----
 
 data_region <- st_read("data/01_maps/02_clean/03_regions/gcrmn_regions.shp") %>% 
   st_drop_geometry() %>% 
   mutate(nb = row_number())
 
-map(data_region$region, ~render_qmd(region_i = ., upload_drive = FALSE))
+# 3. Create the function to render the docx documents ----
+
+render_qmd <- function(region_i, upload_drive = FALSE){
+  
+  require(rmarkdown)
+  require(googledrive)
+  
+  region_name <- str_replace_all(str_replace_all(str_to_lower(region_i), " ", "-"),  "---", "-")
+  
+  nb_chapter <- data_region %>% filter(region == region_i) %>% select(nb) %>% pull()
+  
+  file_name <- paste0(str_pad(nb_chapter, width = 2, pad = "0"), "_", region_name, ".docx")
+  
+  if(file.exists(paste0("doc/", file_name)) == FALSE){
+    
+    rmarkdown::render(input = "code/function/chapter_docx.Rmd", 
+                      output_file = file_name,
+                      output_dir = "doc/",
+                      quiet = TRUE)
+    
+  }
+  
+  if(upload_drive == TRUE){
+    
+    drive_put(media = paste0("doc/", file_name),
+              path = paste0("folder/", file_name))
+    
+  }
+  
+}
+
+# 4. Map over the function ----
+
+render_qmd(region_i = "EAS", upload_drive = FALSE)
+
+#map(data_region$region, ~render_qmd(region_i = ., upload_drive = FALSE))
