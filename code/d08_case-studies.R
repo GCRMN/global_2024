ac4848573917d4b00bf088eb5573a0eaa1d38019
JWicquart <jeremywicquart@gmail.com>
2025-12-11 22:25:21 +0100

    change dataviz for case study 6

--- code/d08_case-studies.R
+++ code/d08_case-studies.R
@@ -16,67 +16,152 @@ source("code/function/theme_graph.R")
 
 # 3. ROPME case study ----
 
-## 3.1 Load data ----
+## 3.1 Countries shapefile ----
+
+data_countries <- read_sf("data/01_maps/01_raw/03_natural-earth/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
+
+## 3.2 2020 data ----
 
 data_2020 <- read.csv2("data/02_misc/03-merge_all_all_all_benthos_NA.csv") %>% 
   filter(Area == "ROPME Area") %>% 
-  rename(decimalLatitude = Latitude, decimalLongitude = Longitude,
-         year = Year, eventDate = Date, datasetID = DatasetID, country = Country) %>% 
-  mutate(month = NA) %>% 
-  group_by(country) %>% 
-  data_descriptors() %>% 
+  select(Longitude, Latitude, Year, Date) %>%
+  distinct() %>% 
+  drop_na(Longitude, Latitude) %>% 
+  group_by(Longitude, Latitude) %>% 
+  summarise(nb_surveys = n()) %>%
   ungroup() %>% 
-  mutate(report = "2020 GCRMN\nreport")
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) 
+
+## 3.3 2025 data -----
 
 load("data/02_misc/data-benthic.RData")
 
 data_2025 <- data_benthic %>% 
   filter(region == "ROPME") %>% 
   filter(country != "Yemen") %>% 
-  group_by(country) %>% 
-  data_descriptors() %>% 
+  select(decimalLatitude, decimalLongitude, year, eventDate, month) %>%
+  distinct() %>% 
+  group_by(decimalLatitude, decimalLongitude) %>% 
+  summarise(nb_surveys = n()) %>%
   ungroup() %>% 
-  mutate(report = "2025 GCRMN\nreport")
+  st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
 
-data_all <- bind_rows(data_2020, data_2025) %>% 
-  select(country, nb_surveys, report) %>% 
-  add_row(country = "Iraq", nb_surveys = 0, report = "2020 GCRMN\nreport") %>% 
-  add_row(country = "Iraq", nb_surveys = 0, report = "2025 GCRMN\nreport") %>% 
-  add_row(country = "Saudi Arabia", nb_surveys = 0, report = "2020 GCRMN\nreport") %>% 
-  mutate(country = str_replace_all(country, "United Arab Emirates", "UAE"),
-         color = case_when(country == "Oman" ~ "black",
-                           country == "UAE" ~ "#FF0000",
-                           country == "Saudi Arabia" ~ "#006C35",
-                           country == "Iran" ~ "#239F40",
-                           country == "Bahrain" ~ "#CE1126",
-                           country == "Kuwait" ~ "black",
-                           country == "Qatar" ~ "#8D1B3D",
-                           country == "Iraq" ~ "black"))
+## 3.4 Create the grid ----
 
-## 3.2 Make the figure ----
+poly <- tibble(lat = c(45, 65),
+               long = c(15, 32)) %>% 
+  st_as_sf(coords = c("lat", "long"), 
+           crs = 4326) %>% 
+  st_bbox() %>% 
+  st_as_sfc()
 
-ggplot(data = data_all, aes(x = report, y = nb_surveys, fill = color, group = country, color = color)) +
-  geom_line(show.legend = FALSE) +
-  geom_point(size = 6, shape = 21, color = "white", show.legend = FALSE) +
-  geom_text(data = data_all %>% filter(report == "2025 GCRMN\nreport"),
-                  aes(x = report, y = nb_surveys, label = country), show.legend = FALSE) +
-  theme_graph() +
-  scale_fill_identity() +
-  scale_color_identity() +
-  scale_x_discrete(expand = c(0.1, 0.5)) +
-  lims(y = c(0, 300)) +
-  labs(y = "Number of surveys", x = NULL)
+data_grid <- st_make_grid(poly, cellsize = 0.4, square = FALSE) %>% 
+  st_as_sf() %>% 
+  mutate(id_poly = row_number())
 
-ggsave("figs/03_case-studies/ropme-raw-text.png", height = 4, width = 6, bg = "transparent", dpi = 300)
+## 3.5 Spatial join ---- 
 
-ggplot(data = data_all, aes(x = report, y = nb_surveys, fill = color, group = country, color = color)) +
-  geom_line(show.legend = FALSE) +
-  geom_point(size = 6, shape = 21, color = "white", show.legend = FALSE) +
-  theme_graph() +
-  scale_fill_identity() +
-  scale_color_identity() +
-  scale_x_discrete(expand = c(0.1, 0.5)) +
-  lims(y = c(0, 300)) +
-  labs(y = "Number of surveys", x = NULL)
+data_2025_grid <- st_join(data_grid, data_2025) %>% 
+  group_by(id_poly) %>% 
+  summarise(nb_surveys_2025 = sum(nb_surveys)) %>% 
+  ungroup()
 
-ggsave("figs/03_case-studies/ropme-raw.png", height = 4, width = 6, bg = "transparent", dpi = 300)
+data_2020_grid <- st_join(data_grid, data_2020) %>% 
+  group_by(id_poly) %>% 
+  summarise(nb_surveys_2020 = sum(nb_surveys)) %>% 
+  ungroup()
+
+data_all <- data_2020_grid %>% 
+  st_drop_geometry() %>% 
+  bind_cols(., data_2025_grid) %>% 
+  select(-"id_poly...3") %>% 
+  rename("id_poly" = "id_poly...1") %>% 
+  mutate(across(c(nb_surveys_2020, nb_surveys_2025), ~replace_na(.x, 0))) %>% 
+  filter(!(nb_surveys_2020 == 0 & nb_surveys_2025 == 0)) %>% 
+  pivot_longer(nb_surveys_2020:nb_surveys_2025, names_to = "report", values_to = "nb_surveys") %>% 
+  mutate(colors = case_when(nb_surveys == 0 ~ "0",
+                            nb_surveys > 0 & nb_surveys <= 5 ~ "1-5",
+                            nb_surveys > 5 & nb_surveys <= 10 ~ "6-10",
+                            nb_surveys > 10 & nb_surveys <= 20 ~ "11-20",
+                            nb_surveys > 20 & nb_surveys <= 45 ~ "21-45",
+                            nb_surveys > 45 & nb_surveys <= 70 ~ "46-70",
+                            nb_surveys > 70 & nb_surveys <= 120 ~ "71-120"),
+         colors = as.factor(colors),
+         colors = factor(colors, levels = c("0", "1-5", "6-10", "11-20", "21-45", "46-70", "71-120"))) %>% 
+  st_as_sf()
+
+rm(data_2020, data_2020_grid, data_2025, data_2025_grid, data_benthic, data_grid, poly)
+
+## 3.6 Make the plots ----
+
+color_scalebar <- "black"
+
+### 3.6.1 2020 data ----
+
+plot_a <- ggplot() +
+  geom_sf(data = data_all %>% filter(report == "nb_surveys_2020"),
+          aes(fill = colors), color = "#747d8c", show.legend = FALSE) +
+  scale_fill_manual(breaks = c("0", "1-5", "6-10", "11-20", "21-45", "46-70", "71-120"),
+                    values = c("#ecf0f1", "#82ccdd", "#3498db", "#fa983a", "#e74c3c", "#c0392b", "#B53471")) +
+  geom_sf(data = data_countries, fill = "#dadfe1", color = "black", linewidth = 0.15) +
+  theme_map() +
+  theme(panel.border = element_rect(fill = NA, color = "black"),
+        panel.background = element_rect(fill = "white"),
+        panel.grid = element_blank(),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        axis.text = element_text(family = font_choose_map, color = "black"),
+        axis.text.y = element_text(angle = 90, hjust = 0.5),
+        axis.text.y.right = element_text(angle = -90, hjust = 0.5),
+        legend.frame = element_rect(fill = "white"),
+        legend.key = element_rect(fill = "white"),
+        legend.text = element_text(family = font_choose_map, size = 6),
+        legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2),
+        plot.title = element_markdown(hjust = 0)) + 
+  coord_sf(xlim = c(45, 65), ylim = c(15, 32),
+           label_axes = list(left = "N", bottom = "E")) +
+  annotation_scale(location = "br",
+                   width_hint = 0.25, text_family = font_choose_map, text_col = color_scalebar,
+                   text_cex = 0.7, style = "bar", line_width = 1, height = unit(0.04, "cm"),
+                   line_col = color_scalebar, pad_x = unit(0.5, "cm"), pad_y = unit(0.5, "cm"),
+                   bar_cols = c(color_scalebar, color_scalebar)) +
+  labs(title = "**A.** <span style='color:#2980b9'>2020</span> GCRMN report")
+
+### 3.6.2 2025 data ----
+
+plot_b <- ggplot() +
+  geom_sf(data = data_all %>% filter(report == "nb_surveys_2025"),
+          aes(fill = colors), color = "#747d8c", show.legend = TRUE) +
+  scale_fill_manual(breaks = c("0", "1-5", "6-10", "11-20", "21-45", "46-70", "71-120"),
+                    values = c("#ecf0f1", "#82ccdd", "#3498db", "#fa983a", "#e74c3c", "#c0392b", "#B53471"),
+                    name = "Number of\nsurveys") +
+  geom_sf(data = data_countries, fill = "#dadfe1", color = "black", linewidth = 0.15) +
+  theme_map() +
+  theme(panel.border = element_rect(fill = NA, color = "black"),
+        panel.background = element_rect(fill = "white"),
+        panel.grid = element_blank(),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        axis.text = element_text(family = font_choose_map, color = "black"),
+        axis.text.y = element_text(angle = 90, hjust = 0.5),
+        axis.text.y.right = element_text(angle = -90, hjust = 0.5),
+        legend.frame = element_rect(fill = "white"),
+        legend.key = element_rect(fill = "white"),
+        legend.direction = "vertical",
+        legend.text = element_text(family = font_choose_map, size = 6),
+        legend.background = element_rect(fill = "white", color = NA, linewidth = 0),
+        plot.title = element_markdown(hjust = 0)) + 
+  coord_sf(xlim = c(45, 65), ylim = c(15, 32),
+           label_axes = list(right = "N", bottom = "E")) +
+  annotation_scale(location = "br",
+                   width_hint = 0.25, text_family = font_choose_map, text_col = color_scalebar,
+                   text_cex = 0.7, style = "bar", line_width = 1, height = unit(0.04, "cm"),
+                   line_col = color_scalebar, pad_x = unit(0.5, "cm"), pad_y = unit(0.5, "cm"),
+                   bar_cols = c(color_scalebar, color_scalebar)) +
+  labs(title = "**B.** <span style='color:#2980b9'>2025</span> GCRMN report")
+
+### 3.6.3 Combine the plots ----
+
+plot_full <- plot_a + plot_b + plot_layout(guides = "collect")
+
+### 3.6.4 Save the plot ----
+
+ggsave("figs/04_case-studies/case-study_6.png", height = 5.5, width = 10.5, bg = "transparent", dpi = 300)
