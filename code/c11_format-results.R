96e0520f6324e7a19a5a0435e87bbaf79cf29576
JWicquart <jeremywicquart@gmail.com>
2026-01-07 15:35:12 +0100

    add subregion names

--- code/c11_format-results.R
+++ code/c11_format-results.R
@@ -1,6 +1,7 @@
 # 1. Load packages ----
 
 library(tidyverse) # Core tidyverse packages
+library(sf)
 
 # 2 Combine and clean model results ----
 
@@ -60,28 +61,28 @@ load("data/11_model-data/data_benthic_prepared.RData")
 
 data_obs <- 
   # ecoregion level
-  data_benthic %>% 
+  data_benthic_prepared %>% 
   select(year, category, region, subregion, ecoregion, measurementValue) %>% 
   group_by(year, category, region, subregion, ecoregion) %>% 
   count(name = "data_obs") %>% 
   ungroup() %>% 
   mutate(level = "ecoregion") %>% 
   # subregion level
-  bind_rows(., data_benthic %>% 
+  bind_rows(., data_benthic_prepared %>% 
               select(year, category, region, subregion, measurementValue) %>% 
               group_by(year, category, region, subregion) %>% 
               count(name = "data_obs") %>% 
               ungroup() %>% 
               mutate(level = "subregion")) %>% 
   # region level
-  bind_rows(., data_benthic %>% 
+  bind_rows(., data_benthic_prepared %>% 
               select(year, category, region, measurementValue) %>% 
               group_by(year, category, region) %>% 
               count(name = "data_obs") %>% 
               ungroup() %>% 
               mutate(level = "region")) %>% 
   # global level
-  bind_rows(., data_benthic %>% 
+  bind_rows(., data_benthic_prepared %>% 
               select(year, category, measurementValue) %>% 
               group_by(year, category) %>% 
               count(name = "data_obs") %>% 
@@ -98,8 +99,18 @@ data_obs <-
            nesting(category, level, region, subregion, ecoregion, first_year, last_year),
            fill = list(data_obs = FALSE)) 
 
-data_models <- left_join(data_models, data_obs) 
-  
-# 4. Export the results ----
+data_models <- left_join(data_models, data_obs)
+
+# 4. Add subregion names ----
+
+data_models <- st_read("data/01_maps/02_clean/04_subregions/gcrmn_subregions.shp") %>% 
+  st_drop_geometry() %>% 
+  rename(subregion = subregn, subregion_name = sbrgn_n) %>% 
+  select(subregion, subregion_name) %>% 
+  distinct() %>% 
+  left_join(data_models, .) %>% 
+  relocate("subregion_name", .after = "subregion")
+
+# 5. Export the results ----
 
 save(data_models, file = "data/model-results.RData")
