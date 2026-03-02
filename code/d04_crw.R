08f5e9982d0971783bb113f1349a9b59fb4698d4
JWicquart <jeremywicquart@gmail.com>
2026-03-02 17:38:34 +0100

    simplify code for x scale labels

--- code/d04_crw.R
+++ code/d04_crw.R
@@ -38,9 +38,9 @@ plot_i <- ggplot(data = data_sst_anom %>% filter(region == "All")) +
   theme(plot.title = element_markdown(size = 17, face = "bold", family = "Open Sans Semibold"),
         plot.subtitle = element_markdown(size = 12)) +
   labs(x = "Year", y = "SST anomaly (°C)") +
-  scale_x_continuous(breaks = c(1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025),
-                     limits = c(1984, 2026),
-                     labels = c("", "1990", "", "2000", "", "2010", "", "2020", "")) +
+  scale_x_continuous(limits = c(1984, 2026),
+                     breaks = seq(1985, 2025, 5),
+                     labels = seq(1985, 2025, 5)) +
   theme_graph() +
   lims(y = c(-1.5, 1.5))
 
@@ -55,9 +55,9 @@ plot_ssta <- function(region_i){
     scale_fill_identity() +
     geom_hline(yintercept = 0) +
     labs(x = "Year", y = "SST anomaly (°C)") +
-    scale_x_continuous(breaks = c(1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025),
-                       limits = c(1984, 2026),
-                       labels = c("1985", "1990", "1995", "2000", "2005", "2010", "2015", "2020", "2025")) +
+    scale_x_continuous(limits = c(1984, 2026),
+                       breaks = seq(1985, 2025, 5),
+                       labels = seq(1985, 2025, 5)) +
     theme_graph() +
     theme(plot.title = element_markdown(size = 17, face = "bold", family = "Open Sans Semibold"),
           plot.subtitle = element_markdown(size = 12),
@@ -116,9 +116,9 @@ plot_i <- ggplot(data = data_dhw_freq %>% filter(region == "All")) +
                                "Alert 4" = palette_second[5],
                                "Alert 5" = "black"),
                     name = "Heat stress level") +
-  scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025),
-                     limits = c(1985, 2026),
-                     labels = c("1990", "1995", "2000", "2005", "2010", "2015", "2020", "2025")) +
+  scale_x_continuous(limits = c(1984, 2026),
+                     breaks = seq(1985, 2025, 5),
+                     labels = seq(1985, 2025, 5)) +
   theme_graph() +
   theme(legend.title.position = "top", legend.title = element_text(hjust = 0.5)) +
   labs(x = "Year", y = "Percentage of coral reefs")
@@ -141,9 +141,9 @@ plot_dhw <- function(region_i){
                                  "Alert 4" = palette_second[5],
                                  "Alert 5" = "black"),
                       name = "Heat stress level") +
-    scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025),
-                       limits = c(1985, 2026),
-                       labels = c("1990", "1995", "2000", "2005", "2010", "2015", "2020", "2025")) +
+    scale_x_continuous(limits = c(1984, 2026),
+                       breaks = seq(1985, 2025, 5),
+                       labels = seq(1985, 2025, 5)) +
     theme_graph() +
     theme(legend.position = "right",
           legend.direction = "vertical",
